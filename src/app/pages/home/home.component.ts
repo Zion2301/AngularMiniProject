b90c175d65ae1c537bcc13e501f82a1f0a45bae7
Zion <omogbemezion@gmail.com>
2025-04-10 22:53:23 +0100

    done

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,28 @@
 import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [MatListModule, MatIconModule,CommonModule,RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements AfterViewInit {
   @ViewChild('videoPlayer') videoPlayer!: ElementRef;
+  isMenuOpen: boolean = false; // To track whether the menu is open or closed
 
   ngAfterViewInit() {
     if (this.videoPlayer) {
       this.videoPlayer.nativeElement.muted = true; // Force mute on load
     }
   }
+
+  // Function to toggle the sliding menu
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen; // Change the menu's state (open/close)
+  }
 }
+
